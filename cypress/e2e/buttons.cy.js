15d708054aa607299608814c7c6a134784fbc014
AlenaD <lenador.18@gmail.com>
2023-05-12 20:28:35 -0500

    add more button actions

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -10,5 +10,12 @@ describe('before each',() => {
         // select a button with text
         cy.contains('Button 2').should('be.visible').click();
         cy.contains('Clicked on button two!').should('be.visible');
+
+        // find element with class attribute and create a list and select the third element from the list
+        cy.get('.btn.btn-primary').then(($buttons) => {
+            cy.wrap($buttons).eq(2).click(); // index of the third element is 2
+            // assert the text
+            cy.contains('Clicked on button three!').should('be.visible');
+        })
     })
 })
