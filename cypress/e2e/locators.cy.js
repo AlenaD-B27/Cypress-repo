44e4df8e67ec4648b2f7477bf50803a025213e56
AlenaD <lenador.18@gmail.com>
2023-05-12 19:15:15 -0500

    add locators strategies

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -7,15 +7,29 @@ describe('before each',() => {
     })
 
     it('different strategies', () => {
+
+        // by css selector
         cy.get('input[name="username"]').type('CydeoStudent');
         cy.get('[type="text"]').clear();
 
-        
+        // by tagname
         cy.get('input').each((item, index, list) => {
             // assert the length of the list is 2
             expect(list).to.have.length(2);
             expect(item).to.have.attr('type');
         })
+
+        // by attribute name
+        cy.get('[type]');
+
+        // by className
+        cy.get('.btn.btn-primary');
+
+        // by id
+        cy.get('#wooden_spoon');
+
+        // what if I want to locate by text
+        cy.get('button').should('contain','Login').click();
     })
 
 
